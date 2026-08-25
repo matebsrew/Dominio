55205acdd3a76f082e6ee1db49fa0305155e5af1
matebsrew <matebsrew@gmail.com>
2026-08-25 14:17:29 -0300

    Atualiza cache para V10 estável

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='treino-v4-9';
+const CACHE='treino-v4-10';
 const ASSETS=['./','./index.html','./app.js','./guides.js','./guides-anatomical-v8.js','./guides-force-v3.js','./guides-init.js','./assets/supino-reto-anatomico.webp','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
